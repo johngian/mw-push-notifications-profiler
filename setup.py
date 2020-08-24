aca06a38f619fa24d0baddf8a9d07ee451ef4344
jgiannelos <jgiannelos@wikimedia.org>
2020-08-24 19:13:06 +0300

    Change python project structure

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import setuptools
 
 setuptools.setup(
-    name="mw_push_profiler",
+    name="mw_push_notifications_profiler",
     version="0.1",
-    packages=setuptools.find_packages(),
+    package_dir={'mw_push_notifications_profiler': 'src'},
+    packages=['mw_push_notifications_profiler'],
     install_requires=["locust", "requests", "python-decouple"],
 )
