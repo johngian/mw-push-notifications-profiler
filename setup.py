ba402751fa96fbb9468d16d86b627da1d20ae69e
jgiannelos <jgiannelos@wikimedia.org>
2020-08-24 19:18:43 +0300

    Black formatter styling fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="mw_push_notifications_profiler",
     version="0.1",
-    package_dir={'mw_push_notifications_profiler': 'src'},
-    packages=['mw_push_notifications_profiler'],
+    package_dir={"mw_push_notifications_profiler": "src"},
+    packages=["mw_push_notifications_profiler"],
     install_requires=["locust", "requests", "python-decouple"],
 )
